44c0fb47ee924ffcd42729009fb14ad87fbcc8a4
jstan-isch <stanleyikegwuonu@gmail.com>
2023-05-28 19:34:21 -0500

    test phase

--- terraform_1/main.tf
+++ terraform_1/main.tf
@@ -1,10 +1,25 @@
-resource "google_pubsub_topic" "pub_sub" {
-    name = "pub_sub"
 
-    labels = {
-        foo = "bar"
-    }
-
-    message_retention_duration = "86600s"
-  
+resource "google_compute_network" "vpc_network" {
+  name    = "tf-network"
+  project = "clgcporg8-021"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  project      = "clgcporg8-021"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
